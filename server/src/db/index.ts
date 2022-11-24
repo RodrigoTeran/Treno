1e1efb7555251daeeecf58b3fc18559c890ee094
RodrigoTeran <rodrigo.teranhernandez@gmail.com>
2022-11-24 13:56:28 -0600

    fix: edit env variables pg database

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,9 +1,9 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT)
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: parseInt(process.env.PGPORT)
 });
